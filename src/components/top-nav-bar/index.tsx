2daab8877cbd81f00f38e2a0a8d4ec43794442e6
chhotu1 <chhotukumarsow@gmail.com>
2024-05-04 23:12:20 +0530

    add nav bar

--- src/components/top-nav-bar/index.tsx
+++ src/components/top-nav-bar/index.tsx
@@ -36,7 +36,7 @@ const TopNavbar = () => {
                 alt="FlowBite Logo"
               />
               <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                Flowbite
+                Admin Template
               </span>
             </a>
           </div>
