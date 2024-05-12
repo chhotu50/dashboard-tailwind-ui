01310b59b11033dac0a7eae045a77f106a213952
chhotu1 <chhotukumarsow@gmail.com>
2024-05-12 22:54:56 +0530

    add color

--- src/components/label/index.tsx
+++ src/components/label/index.tsx
@@ -6,7 +6,7 @@ const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
     return (
       <label
         ref={ref}
-        className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70  ${className}`}
+        className={`dark:text-gray-600 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70  ${className}`}
         {...props}
       />
     );
